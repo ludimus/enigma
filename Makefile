4ca9434191d39f344d88db65acc37a779a400813
ludimus <alwin.mob@gmail.com>
2017-08-24 13:25:56 +0200

    added encryptm3

--- Makefile
+++ Makefile
@@ -1,41 +1,22 @@
-LINK_TARGET = enigmaM3
-OBJS =  enigmam3.o rotor.o reflector.o plugboard.o
-REBUILDABLES = $(OBJS) $(LINK_TARGET)
+TARGETS = enigmaM3 encryptM3
+CLASSES = rotor.o reflector.o plugboard.o
 CPPFLAGS=-g -std=c++11
 
-# Header file dependencies
+# Header file dependencies for classes
 rotor.o : rotor.h
 reflector.o : reflector.h
 plugboard.o : plugboard.h
 
-# The rule for "all" is used to incrementally build your system.
-# It does this by expressing a dependency on the results of that system,
-# which in turn have their own rules and dependencies.
+all : enigmaM3 encryptM3
 
-all : $(LINK_TARGET)
-
-# Here is a Rule that uses some built-in Make Macros in its command:
-# $@ expands to the rule's target, in this case "test_me.exe".
-# $^ expands to the rule's dependencies, in this case the three files
-# main.o, test1.o, and  test2.o.
-
-$(LINK_TARGET) : $(OBJS)
+encryptM3 : encryptm3.o $(CLASSES)
 	g++ -g -o $@ $^
 
-# Here is a Pattern Rule, often used for compile-line.
-# It says how to create a file with a .o suffix, given a file with a .cpp suffix.
-# The rule's command uses some built-in Make Macros:
-# $@ for the pattern-matched target
-# $< for the pattern-matched dependency
+enigmaM3 : enigmam3.o $(CLASSES)
+	g++ -g -o $@ $^
 
 %.o : %.cpp
 	g++ $(CPPFLAGS) -o $@ -c $<  
-  
-# Here is a simple Rule (used for "cleaning" your build environment).
-# It has a Target named "clean" (left of the colon ":" on the first line),
-# no Dependencies (right of the colon),
-# and two Commands (indented by tabs on the lines that follow).
-# The space before the colon is not required but added here for clarity.
 
 clean : 
-	rm -f $(REBUILDABLES)
+	rm -f $(CLASSES) $(TARGETS)
---
+++ encryptm3.cpp
@@ -0,0 +1,190 @@
+#include <iostream>
+#include <unistd.h>
+#include <cstdlib>
+#include "rotor.h"
+#include "reflector.h"
+#include "plugboard.h"
+
+
+using namespace std;
+
+void rotate(rotor& R1,rotor& R2, rotor& R3){
+
+    R3.step();                            // Rotate fast always
+    if (R3.over_notch())
+          R2.step();                      // Rotate middle when fast is notched
+    else 
+       if (R2.on_notch()){                // Check double_step scenario
+            R2.step();                    // when true rotate middle and slow rotor
+            R1.step();
+        }
+
+}
+
+void set_plugs(plugboard& PB, char* plugs){
+    
+    string plugfailed ="-p Incorrect Plugsettings example: AB:EF:TH to swap AB EF TH";
+    
+    char a,b,d;
+    
+    for (int i=0;i<30;i+=3){
+        a=toupper(plugs[i]);    
+        b=toupper(plugs[i+1]);
+        d=plugs[i+2];
+        if ((a<'A')|| (a>'Z')||(b<'A')|| (b>'Z')){
+            cout<<plugfailed<<endl;
+            exit(1);                
+        }
+        PB.swap((int)(a-65),(int)(b-65));
+        if(d!=':') i=30;  
+    }
+    
+}
+
+void set_key(rotor& R1, rotor& R2, rotor& R3, char* rp){
+    
+    string keyfailed ="-k Incorrect messageKey example: HFA ";
+    
+    char rtype,rpos;
+    
+    for (int i=0;i<3;i++){
+        rpos=toupper(rp[i]);    
+        if ((rpos<'A')|| (rpos>'Z')){
+            cout<<keyfailed<<endl;
+            exit(1);                
+        }
+        if(i==0) R1.setpos((int)(rpos-65));
+        if(i==1) R2.setpos((int)(rpos-65));
+        if(i==2) R3.setpos((int)(rpos-65));
+                
+    }
+    
+}
+
+void set_rotors(rotor& R1, rotor& R2, rotor& R3, char* rp){
+    
+    string rotorfailed ="-w Incorrect Rotorsetting, example: 1A2B4K for rotors I II IV with ringstellung ABK";
+    
+    char rtype,rpos;
+    
+    for (int i=0;i<6;i+=2){
+        rtype=rp[i];
+        rpos=toupper(rp[i+1]);    
+        if(rtype<'1' || rtype>'8'){
+            cout<<rotorfailed<<endl;
+            exit(1);                
+        }
+        if ((rpos<'A')|| (rpos>'Z')){
+            cout<<rotorfailed<<endl;
+            exit(1);                
+        }
+        if(i==0) R1.init((int)(rtype-49),(int)(rpos-65));
+        if(i==2) R2.init((int)(rtype-49),(int)(rpos-65));
+        if(i==4) R3.init((int)(rtype-49),(int)(rpos-65));
+                
+    }
+    
+}
+
+void set_ukw(reflector& RF,char* rp){
+    
+    
+    char rfc=toupper(rp[0]);
+        if((rfc<'A') || (rfc>'C')){
+            cout<<"-u Enigma M3 reflector options: [a-c][A-C] "<<endl;
+            exit(1);
+        }
+        RF.init((int)(rfc-65));
+}
+
+char encode_char(char c, rotor& R1, rotor& R2, rotor& R3, reflector RF, plugboard& PB){
+
+    int P=(int)c -65;       //convert char to int, enigma classes uses integers
+
+    P=PB.pass(P);
+    P=R3.encode(P); 
+    P=R2.encode(P); 
+    P=R1.encode(P); 
+    P=RF.reflect(P); 
+    P=R1.reverse(P); 
+    P=R2.reverse(P); 
+    P=R3.reverse(P);
+    P=PB.pass(P);
+    
+    return (char)(P+65);
+}
+
+void scramble_message(rotor& R1, rotor& R2, rotor& R3, reflector RF,plugboard& PB){
+    
+    string m;
+    char c;
+    cout<<endl<<"Enter  message: ";
+    cin.ignore();
+    getline(std::cin,m);
+    cout<<endl<<"Enigma message: ";
+    
+    for (int i=0;i<m.length();i++)
+        if(isalpha(m.at(i))){
+            c=toupper(m.at(i));
+            rotate(R1,R2,R3); 
+            cout<<encode_char(c,R1,R2,R3,RF,PB);
+        }
+        else cout<<" ";
+        cout<<endl;
+}
+
+void show_usage(){
+    cout<<"usage"<<endl;
+}
+
+int main(int argc, char* argv[])
+{
+    // create Enigma M3 with defaults:
+    // Rotors I II III, ringstelling AAA, messagekey AAA, plugs none
+    
+    rotor R1(0,0);
+    rotor R2(1,0);
+    rotor R3(2,0);
+    R1.setpos(0);
+    R2.setpos(0);
+    R3.setpos(0);
+
+    reflector RF(1);
+    plugboard PB;
+    
+    // process command line arguments
+    int c;
+    while( ( c = getopt(argc, argv, "u:w:k:p:h") ) != -1 )
+        {
+            switch(c)
+            {
+                case 'u': set_ukw(RF,optarg);break;
+                case 'w': set_rotors(R1,R2,R3,optarg);break;
+                case 'k': set_key(R1,R2,R3,optarg);break;
+                case 'p': set_plugs(PB,optarg);break;   
+                case 'h': show_usage();break;
+            }
+        }
+    //process stdin
+    string m;
+    char p;
+    while(getline(cin, m)) {
+        for (int i=0;i<m.length();i++)
+            if(isalpha(m.at(i))){
+                p=toupper(m.at(i));
+                rotate(R1,R2,R3); 
+                cout<<encode_char(p,R1,R2,R3,RF,PB);
+                //cout<<p;
+            }
+            else cout<<" ";
+    cout<<endl;
+    }
+
+    
+return 0;
+}
+
+
+
+
+
